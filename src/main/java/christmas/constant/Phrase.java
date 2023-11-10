28d28fa782d73a7ff9fa85f1eab91af06a4d67aa
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-11-10 15:57:18 +0900

    feat(constant): Phrase 클래스 생성
    - 구문을 상수로 정의

---
+++ src/main/java/christmas/constant/Phrase.java
@@ -0,0 +1,15 @@
+package christmas.constant;
+
+public final class Phrase {
+    public static final String WELCOME = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String ASK_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String ASK_ORDER_COUNT = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String EVENT_PREVIEW = "12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    public static final String ORDER_MENU = "\n<주문 메뉴>";
+    public static final String TOTAL_COST_BEFORE_DISCOUNT = "\n<할인 전 총주문 금액>";
+    public static final String TOTAL_COST_AFTER_DISCOUNT = "\n<할인 후 예상 결제 금액>";
+    public static final String FREE_GIFT = "\n<증정 메뉴>";
+    public static final String BENEFIT = "\n<혜택 내역>";
+    public static final String BENEFIT_COST = "\n<총혜택 금액>";
+    public static final String EVENT_BADGE = "\n<12월 이벤트 배지>";
+}
