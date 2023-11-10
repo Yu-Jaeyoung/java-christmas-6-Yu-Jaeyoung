e0ea4e97354d0d5710976868b0776bb57109d5b7
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-11-10 18:29:05 +0900

    feat(exception): Exception 클래스 메소드 추가
    - 예외 상황 정의
      - 숫자 외 입력
      - 1보다 작거나 31보다 큰 입력

--- src/main/java/christmas/exception/Exception.java
+++ src/main/java/christmas/exception/Exception.java
@@ -12,4 +12,24 @@ public class Exception {
             throw new IllegalArgumentException("[ERROR] 공백이 포함된 입력");
         }
     }
+
+    public static void notANumber(final String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException("[ERROR] 숫자 외 입력");
+        }
+    }
+
+    public static void notInRange(final String input) {
+        final int number = Integer.parseInt(input);
+
+        if (number < 1) {
+            throw new IllegalArgumentException("[ERROR] 1보다 작은 입력");
+        }
+
+        if (number > 31) {
+            throw new IllegalArgumentException("[ERROR] 31보다 큰 입력");
+        }
+    }
 }
