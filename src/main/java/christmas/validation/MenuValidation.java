76301711caded1af86a125b367879f7288d1d7e4
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-11-11 00:39:35 +0900

    feat(validation): MenuValidation 클래스 생성
    - MenuException을 활용하여 validation을 진행하는 클래스 생성
      - Menu와 관련된 입력이 정상적으로 처리되었는지 판단
        - validate();
          - MenuException.blank();
          - MenuException.containsBlank();
          - MenuException.nothingBeforeComma();
          - MenuException.nothingAfterComma();
          - MenuException.noNumberBeforeComma();
          - MenuException.noHyphen();
        - validateAfterSplitWithComma();
          - MenuException.noHyphen();
          - MenuException.nothingBeforeHyphen();
          - MenuException.nothingAfterHyphen();
        - validateAfterSplitWithHyphen();
          - MenuException.duplicatedMenu();
          - MenuException.notInMenu();
          - MenuException.notInRangeNumber();

---
+++ src/main/java/christmas/validation/MenuValidation.java
@@ -0,0 +1,35 @@
+package christmas.validation;
+
+import christmas.exception.MenuException;
+import christmas.util.Splitter;
+
+public class MenuValidation {
+    public static void validate(final String input) {
+        MenuException.blank(input);
+        MenuException.containsBlank(input);
+        MenuException.nothingBeforeComma(input);
+        MenuException.nothingAfterComma(input);
+        MenuException.noNumberBeforeComma(input);
+        MenuException.noHyphen(input);
+    }
+
+    public static void validateAfterSplitWithComma(final String input) {
+        final String[] menus = Splitter.splitWithComma(input);
+
+        MenuException.noHyphen(menus);
+        MenuException.nothingBeforeHyphen(menus);
+        MenuException.nothingAfterHyphen(menus);
+    }
+
+    public static void validateAfterSplitWithHyphen(final String input) {
+        final String[] menus = Splitter.splitWithComma(input);
+
+        MenuException.duplicatedMenu(menus);
+
+        for (final String menu : menus) {
+            final String[] order = Splitter.splitWithHyphen(menu);
+            MenuException.notInMenu(order[0]);
+            MenuException.notInRangeNumber(order[1]);
+        }
+    }
+}
