6d1192b59b8ec19edb75c4ae551de01d2c15ddc6
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-11-12 03:08:58 +0900

    feat(view): Output 클래스 메소드 추가 정의
    - 할인 전 총 주문 금액을 출력하는 메소드 추가
      - OutputView#totalCostBeforeDiscount();

--- src/main/java/christmas/launcher/EventPlannerLauncher.java
+++ src/main/java/christmas/launcher/EventPlannerLauncher.java
@@ -1,14 +1,21 @@
 package christmas.launcher;
 
+import christmas.constant.Menu;
 import christmas.constant.Phrase;
 import christmas.controller.DateController;
 import christmas.controller.MenuController;
+import christmas.domain.Cost;
 import christmas.view.OutputView;
+
+import java.util.Map;
 
 public class EventPlannerLauncher {
     public void run() {
         System.out.println(Phrase.WELCOME);
         DateController.readDate();
-        OutputView.orderMenu(MenuController.readMenus());
+        final Map<Menu, Integer> OrderHistory = MenuController.readMenus();
+
+        OutputView.orderMenu(OrderHistory);
+        OutputView.totalCostBeforeDiscount(Cost.totalCost(OrderHistory));
     }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,17 +2,25 @@ package christmas.view;
 
 import christmas.constant.Menu;
 import christmas.constant.Phrase;
+import christmas.util.Formatter;
+
 import java.util.Map;
 
 public class OutputView {
     public static void orderMenu(final Map<Menu, Integer> orderHistory) {
         System.out.println(Phrase.EVENT_PREVIEW);
         System.out.println(Phrase.ORDER_MENU);
+
         for (final Map.Entry<Menu, Integer> menu : orderHistory.entrySet()) {
             final String menuName = menu.getKey().getMenuName();
             final int quantity = menu.getValue();
 
             System.out.println(menuName + " " + quantity + "개");
         }
+    }
+
+    public static void totalCostBeforeDiscount(final int totalCost) {
+        System.out.println(Phrase.TOTAL_COST_BEFORE_DISCOUNT);
+        System.out.println(Formatter.decimalFormatter(totalCost) + "원");
     }
 }
