289ed9507e6cd1bbcbeba5fbff28de5efb7e6cb8
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-11-13 03:16:44 +0900

    feat(domain): Cost 클래스 수정
    - 할인 정보를 가지고 있는 클래스로 수정
    - 생성자를 통해 할인 전 총금액을 초기화
      - initializeTotalCostBeforeDiscount();
    - 이벤트와 관련된 변수 정의
      - christmasDiscount
      - weekendDiscount
      - weekdayDiscount
      - specialDiscount
      - freeGiftDiscount
    - getter, setter 정의
      - 지양해야하는 것은 인지
      - 수정 고민 필요

--- src/main/java/christmas/domain/Cost.java
+++ src/main/java/christmas/domain/Cost.java
@@ -1,19 +1,85 @@
 package christmas.domain;
 
 import christmas.constant.Menu;
+
 import java.util.Map;
 
 public class Cost {
-    public static int totalCost(final Map<Menu, Integer> orderHistory) {
+    private final Order order;
+    private final int totalCostBeforeDiscount;
+    private int christmasDiscount = 0;
+    private int weekendDiscount = 0;
+    private int weekdayDiscount = 0;
+    private int specialDiscount = 0;
+    private int freeGiftDiscount = 0;
+
+    public Cost(final Order order) {
+        this.order = order;
+        this.totalCostBeforeDiscount = initializeTotalCostBeforeDiscount(order);
+    }
+
+    public final int initializeTotalCostBeforeDiscount(final Order order) {
         int totalCost = 0;
 
-        for (final Map.Entry<Menu, Integer> menu : orderHistory.entrySet()) {
+        for (final Map.Entry<Menu, Integer> menu : order.getOrderHistory().entrySet()) {
             final Menu menuName = menu.getKey();
             final int quantity = menu.getValue();
 
-            totalCost = totalCost + (menuName.getPrice() * quantity);
+            totalCost += (menuName.getPrice() * quantity);
         }
 
         return totalCost;
+    }
+
+    public final int getTotalCostBeforeDiscount() {
+        return totalCostBeforeDiscount;
+    }
+
+    public final int getTotalCostAfterDiscount() {
+        return totalCostBeforeDiscount + getBenefitCost() - freeGiftDiscount;
+    }
+
+    public final int getChristmasDiscount() {
+        return christmasDiscount;
+    }
+
+    public final int getWeekendDiscount() {
+        return weekendDiscount;
+    }
+
+    public final int getWeekdayDiscount() {
+        return weekdayDiscount;
+    }
+
+    public final int getSpecialDiscount() {
+        return specialDiscount;
+    }
+
+    public final int getFreeGiftDiscount() {
+        return freeGiftDiscount;
+    }
+
+    public final int getBenefitCost() {
+        return christmasDiscount + weekendDiscount + weekdayDiscount + specialDiscount + freeGiftDiscount;
+    }
+
+    public final void applyWeekendDiscount() {
+        weekendDiscount += -2_023 * order.getMainQuantity();
+    }
+
+    public final void applyWeekdayDiscount() {
+        weekdayDiscount += -2_023 * order.getDessertQuantity();
+    }
+
+    public final void applySpecialDiscount() {
+        specialDiscount += -1_000;
+    }
+
+    public final void applyChristmasDiscount(final int date) {
+        christmasDiscount += -1_000 - (100 * (date - 1));
+    }
+
+    public final void applyFreeGiftDiscount() {
+        freeGiftDiscount += -25_000;
     }
 }
