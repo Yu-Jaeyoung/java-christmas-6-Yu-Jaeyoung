edf2da000a823113e13b7a6ba82b328677564c93
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-11-12 23:34:41 +0900

    feat(constant): Phrase 클래스 업데이트
    - Phrase 클래스 상수 정의 추가
    - README.md 업데이트

--- src/main/java/christmas/constant/Phrase.java
+++ src/main/java/christmas/constant/Phrase.java
@@ -14,4 +14,12 @@ public final class Phrase {
     public static final String BENEFIT_COST = "\n<총혜택 금액>";
     public static final String EVENT_BADGE = "\n<12월 이벤트 배지>";
     public static final String NOTHING = "없음\n";
+    public static final String WEEKDAY_DISCOUNT = "평일 할인: ";
+    public static final String WEEKEND_DISCOUNT = "주말 할인: ";
+    public static final String SPECIAL_DISCOUNT = "특별 할인: ";
+    public static final String CHRISTMAS_DISCOUNT = "크리스마스 디데이 할인: ";
+    public static final String FREE_GIFT_EVENT = "증정 이벤트: ";
+    public static final String EVENT_BADGE_SANTA = "산타";
+    public static final String EVENT_BADGE_TREE = "트리";
+    public static final String EVENT_BADGE_STAR = "별";
 }
