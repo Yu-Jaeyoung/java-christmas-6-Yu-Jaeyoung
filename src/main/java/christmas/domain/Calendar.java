245ae3ce78483f9b3912dba24c12ba6bff39cefa
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-11-12 16:56:02 +0900

    feat(domain): Calendar 클래스 생성
    - 이벤트 혜택 대상 확인을 위한 Calendar 클래스 정의

---
+++ src/main/java/christmas/domain/Calendar.java
@@ -0,0 +1,26 @@
+package christmas.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public final class Calendar {
+    private final int date;
+    private final List<Integer> weekend = Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30);
+    private final List<Integer> starDay = Arrays.asList(3, 10, 17, 24, 25, 31);
+
+    public Calendar(final int date) {
+        this.date = date;
+    }
+
+    public final boolean isWeekend() {
+        return weekend.contains(this.date);
+    }
+
+    public final boolean isStarDay() {
+        return starDay.contains(this.date);
+    }
+
+    public final boolean isChristmasDDay() {
+        return this.date < 26;
+    }
+}
