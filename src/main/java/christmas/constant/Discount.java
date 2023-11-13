a54af1a999a73c3e3c770459c45a64bbf195e6c5
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-11-14 06:37:42 +0900

    refactor(constant): Discount Enum 내용 수정
    - Phrase에 있는 Discount 관련 내용을 이동 진행

--- src/main/java/christmas/constant/Discount.java
+++ src/main/java/christmas/constant/Discount.java
@@ -1,17 +1,23 @@
 package christmas.constant;
 
 public enum Discount {
-    CHRISTMAS(1_000),
-    WEEKEND(2_023),
-    WEEKDAY(2_023),
-    SPECIAL(1_000),
-    FREE_GIFT(25_000);
+    CHRISTMAS("크리스마스 디데이 할인: ", 1_000),
+    WEEKEND("주말 할인: ", 2_023),
+    WEEKDAY("평일 할인: ", 2_023),
+    SPECIAL("특별 할인: ", 1_000),
+    FREE_GIFT("증정 이벤트: ", 25_000);
 
-    Discount(final int discountValue) {
+    Discount(final String discountPhrase, final int discountValue) {
+        this.discountPhrase = discountPhrase;
         this.discountValue = discountValue;
     }
 
+    private final String discountPhrase;
     private final int discountValue;
+
+    public String getDiscountPhrase() {
+        return discountPhrase;
+    }
 
     public final int getDiscountValue() {
         return discountValue;
--- src/main/java/christmas/constant/Phrase.java
+++ src/main/java/christmas/constant/Phrase.java
@@ -1,7 +1,6 @@
 package christmas.constant;
 
 public final class Phrase {
-    public static final String WELCOME = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     public static final String ASK_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     public static final String ASK_ORDER_COUNT = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     public static final String ORDER_MENU = "\n<주문 메뉴>";
@@ -13,11 +12,6 @@ public final class Phrase {
     public static final String BENEFIT_COST = "\n<총혜택 금액>";
     public static final String EVENT_BADGE = "\n<12월 이벤트 배지>";
     public static final String NOTHING = "없음\n";
-    public static final String WEEKDAY_DISCOUNT = "평일 할인: ";
-    public static final String WEEKEND_DISCOUNT = "주말 할인: ";
-    public static final String SPECIAL_DISCOUNT = "특별 할인: ";
-    public static final String CHRISTMAS_DISCOUNT = "크리스마스 디데이 할인: ";
-    public static final String FREE_GIFT_EVENT = "증정 이벤트: ";
     public static final String EVENT_BADGE_SANTA = "산타";
     public static final String EVENT_BADGE_TREE = "트리";
     public static final String EVENT_BADGE_STAR = "별";
