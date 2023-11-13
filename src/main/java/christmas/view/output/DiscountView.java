c54d01e59766efcd451d6ee49d656f6410ac5db9
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-11-14 06:47:43 +0900

    refactor(view): DiscountView 메소드 간소화
    - discountView() 단일 메소드로 간소화 진행

--- src/main/java/christmas/view/output/DiscountView.java
+++ src/main/java/christmas/view/output/DiscountView.java
@@ -1,41 +1,13 @@
 package christmas.view.output;
 
-import christmas.constant.Phrase;
+import christmas.constant.Discount;
 import christmas.util.Formatter;
 
 public class DiscountView {
-    public static void weekendDiscount(final int discount) {
-        if (discount != 0) {
-            System.out.print(Phrase.WEEKEND_DISCOUNT);
-            System.out.println(Formatter.decimalFormatter(discount) + "원");
-        }
-    }
 
-    public static void weekdayDiscount(final int discount) {
-        if (discount != 0) {
-            System.out.print(Phrase.WEEKDAY_DISCOUNT);
-            System.out.println(Formatter.decimalFormatter(discount) + "원");
-        }
-    }
-
-    public static void specialDiscount(final int discount) {
-        if (discount != 0) {
-            System.out.print(Phrase.SPECIAL_DISCOUNT);
-            System.out.println(Formatter.decimalFormatter(discount) + "원");
-        }
-    }
-
-    public static void christmasDiscount(final int discount) {
-        if (discount != 0) {
-            System.out.print(Phrase.CHRISTMAS_DISCOUNT);
-            System.out.println(Formatter.decimalFormatter(discount) + "원");
-        }
-    }
-
-    public static void freeGiftDiscount(final int discount) {
-        if (discount != 0) {
-            System.out.print(Phrase.FREE_GIFT_EVENT);
-            System.out.println(Formatter.decimalFormatter(discount) + "원");
+    public static void discountView(final Discount discount, final int discountValue) {
+        if (discountValue != 0) {
+            System.out.printf("%s%s원\n", discount.getDiscountPhrase(), Formatter.decimalFormatter(discountValue));
         }
     }
 }
