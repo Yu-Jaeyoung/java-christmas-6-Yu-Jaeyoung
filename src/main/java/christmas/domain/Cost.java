8b86f48aac4093f6c4c0e4c53f451f476cbe62d2
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-11-14 09:25:10 +0900

    refactor(domain): Cost 클래스 수정
    - Cost 클래스에서 기존 Order의 메소드를 가지게함
      - Order을 생성자로 받은 뒤 활용할 수 있게 수정

--- src/main/java/christmas/domain/Cost.java
+++ src/main/java/christmas/domain/Cost.java
@@ -7,12 +7,14 @@ import java.util.Map;
 
 public class Cost {
     private final int totalCost;
+    private final Order order;
 
     public Cost(final Order order) {
-        this.totalCost = initializeTotalCost(order);
+        this.order = order;
+        this.totalCost = initializeTotalCost();
     }
 
-    private int initializeTotalCost(final Order order) {
+    private int initializeTotalCost() {
         int totalCost = 0;
 
         for (final Map.Entry<Menu, Integer> menu : order.getOrderHistory().entrySet()) {
@@ -33,11 +35,35 @@ public class Cost {
         return totalCost >= 10_000;
     }
 
+    public final int getMainQuantity() {
+        int mainQuantity = 0;
+
+        for (final Map.Entry<Menu, Integer> menu : order.getOrderHistory().entrySet()) {
+            if (menu.getKey().getCourse().equals("main")) {
+                mainQuantity += menu.getValue();
+            }
+        }
+
+        return mainQuantity;
+    }
+
+    public final int getDessertQuantity() {
+        int dessertQuantity = 0;
+
+        for (final Map.Entry<Menu, Integer> menu : order.getOrderHistory().entrySet()) {
+            if (menu.getKey().getCourse().equals("dessert")) {
+                dessertQuantity += menu.getValue();
+            }
+        }
+
+        return dessertQuantity;
+    }
+
     public final void totalCostView() {
         CostView.totalCostBeforeDiscount(this.totalCost);
     }
 
-    public final void totalPayment(final int totalDiscount) {
+    public final void totalPaymentView(final int totalDiscount) {
         if (isFreeGiftTarget()) {
             CostView.totalCostAfterDiscount(this.totalCost + totalDiscount + 25_000);
             return;
